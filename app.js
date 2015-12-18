c89a62768a0f27d73548367b8a3fd96f4ff75037
Semere <BINI>
2015-12-18 16:41:53 +0100

    project modification

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-$.getJSON('https://s.idsympa.com/u-5.json', function(data){
-var ele = $("[elt]"); //to get all elements in attr -- elt
-for(var i=0; i<ele.length;i++){   // une boucle avec chacun de ses élements
-	
-	var attribut = $(ele[i]).attr("elt"); // lire l'attribut elt de cet élément
-    
-    var valuer = data[attribut];
-    
-    $(ele[i]).html(valuer);
-}
-});
 
 $.getJSON('user-5.json', function(data) { // to make mustach
-    var template = $('#template').html();
-    var html = Mustache.to_html(template, data);
-    $('#cible').html(html);
+	var template = $('#template').html();
+	var html = Mustache.to_html(template, data);
+	$('body').html(html);
+});
+
+
+$(document).ready(function() {
+	$("body").on('click','.modalclick', function(event) {
+		event.preventDefault();
+		$('#overlay').show();
+	});
+	
+	$('body').on('click','.closed', function(event) {
+		event.preventDefault();
+		$('#overlay').hide();
+	});
 });
 
 
@@ -104,35 +106,35 @@ $.getJSON('user-5.json', function(data) { // to make mustach
 
 $(document).ready(function() {
 	$(function(){
- 
-	$(document).on( 'scroll', function(){
- 
-		if ($(window).scrollTop() > 100) {
-			$('.scroll-top-wrapper').addClass('show');
-		} else {
-			$('.scroll-top-wrapper').removeClass('show');
-		}
+
+		$(document).on( 'scroll', function(){
+
+			if ($(window).scrollTop() > 100) {
+				$('.scroll-top-wrapper').addClass('show');
+			} else {
+				$('.scroll-top-wrapper').removeClass('show');
+			}
+		});
 	});
-});
-$(function(){
- 
-	$(document).on( 'scroll', function(){
- 
-		if ($(window).scrollTop() > 100) {
-			$('.scroll-top-wrapper').addClass('show');
-		} else {
-			$('.scroll-top-wrapper').removeClass('show');
-		}
+	$(function(){
+
+		$(document).on( 'scroll', function(){
+
+			if ($(window).scrollTop() > 100) {
+				$('.scroll-top-wrapper').addClass('show');
+			} else {
+				$('.scroll-top-wrapper').removeClass('show');
+			}
+		});
+
+		$('.scroll-top-wrapper').on('click', scrollToTop);
 	});
- 
-	$('.scroll-top-wrapper').on('click', scrollToTop);
-});
- 
-function scrollToTop() {
-	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
-	element = $('body');
-	offset = element.offset();
-	offsetTop = offset.top;
-	$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
-}
+
+	function scrollToTop() {
+		verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+		element = $('body');
+		offset = element.offset();
+		offsetTop = offset.top;
+		$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+	}
 });
